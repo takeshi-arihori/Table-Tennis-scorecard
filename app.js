9e0a9adf743db1cdb85b97da5f4af949ce33c4bb
Takeshi Arihori <xxxarihoriixxx@gmail.com>
2023-03-18 09:48:32 +0900

    リファクタリング

--- app.js
+++ app.js
@@ -1,49 +1,47 @@
 'use strict';
 
-// button
-const p1Btn = document.querySelector('#p1Button');
-const p2Btn = document.querySelector('#p2Button');
-// display
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
+const p1 = {
+  score: 0,
+  button: document.querySelector('#p1Button'),
+  display: document.querySelector('#p1Display'),
+}
+
+const p2 = {
+  score: 0,
+  button: document.querySelector('#p2Button'),
+  display: document.querySelector('#p2Display'),
+}
+
 // reset button
 const resetBtn = document.querySelector('#reset');
 // select
 const winningScoreSelect = document.querySelector('#winningScore');
 
-let p1Score = 0;
-let p2Score = 0;
 let winningScore = 3;
 let isGameOver = false;
 
-p1Btn.addEventListener('click', (e) => {
-  e.preventDefault();
+function updateScores(player, opponent) {
   if (!isGameOver) {
-    p1Score++;
-    p1Display.innerHTML = p1Score;
-    if (p1Score === winningScore) {
+    player.score++;
+    player.display.textContent = player.score;
+    if (player.score === winningScore) {
       isGameOver = true;
-      p1Display.classList.add('has-text-success');
-      p2Display.classList.add('has-text-danger');
-      p1Btn.disabled = true;
-      p2Btn.disabled = true;
+      player.display.classList.add('has-text-success');
+      opponent.display.classList.add('has-text-danger');
+      player.button.disabled = true;
+      opponent.button.disabled = true;
     }
   }
+}
+
+p1.button.addEventListener('click', (e) => {
+  e.preventDefault();
+  updateScores(p1, p2);
 });
 
-p2Btn.addEventListener('click', (e) => {
+p2.button.addEventListener('click', (e) => {
   e.preventDefault();
-  if (!isGameOver) {
-    p2Score++;
-    p2Display.innerHTML = p2Score;
-    if (p2Score === winningScore) {
-      isGameOver = true;
-      p1Display.classList.add('has-text-danger');
-      p2Display.classList.add('has-text-success');
-      p1Btn.disabled = true;
-      p2Btn.disabled = true;
-    }
-  }
+  updateScores(p2, p1);
 });
 
 winningScoreSelect.addEventListener('change', function () {
@@ -56,12 +54,10 @@ resetBtn.addEventListener('click', reset);
 
 function reset() {
   isGameOver = false;
-  p1Score = 0;
-  p2Score = 0;
-  p1Display.innerHTML = p1Score;
-  p2Display.innerHTML = p2Score;
-  p1Display.classList.remove('has-text-success', 'has-text-danger');
-  p2Display.classList.remove('has-text-success', 'has-text-danger');
-  p1Btn.disabled = false;
-  p2Btn.disabled = false;
+  for(let p of [p1, p2]){
+    p.score = 0;
+    p.display.textContent = 0;
+    p.display.classList.remove('has-text-success', 'has-text-danger');
+    p.button.disabled = false;
+  }
 }
